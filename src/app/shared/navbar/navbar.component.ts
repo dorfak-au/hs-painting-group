f37c333b3b0a879bb7df9098b4b9b585ee071201
bmotevalli <b.motevalli@gmail.com>
2025-04-28 10:13:24 +0800

    init angular project.

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterModule, MatToolbarModule, MatButtonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {}
