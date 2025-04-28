2a395ec45e5c3cf98629a49dca7e11594c1d1689
bmotevalli <b.motevalli@gmail.com>
2025-04-28 12:59:34 +0800

    adding Poppins font and material style.

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
-import { Component } from "@angular/core";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule } from "@angular/material/button";
-import { RouterModule } from "@angular/router";
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: "app-navbar",
+  selector: 'app-navbar',
   imports: [RouterModule, MatToolbarModule, MatButtonModule],
-  templateUrl: "./navbar.component.html",
-  styleUrl: "./navbar.component.css",
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {}
