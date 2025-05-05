01a5d4894fdf6a8bbdf257b98598ba2eb9ec45da
Ben Motevalli <b.motevalli@gmail.com>
2025-05-05 03:09:10 -0700

    adding sending email, FAQ, and firebase deploy.

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule, MatToolbarModule, MatButtonModule],
+  imports: [RouterModule,  MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { EmailService } from '../../shared/email.service';
 import { CommonModule } from '@angular/common';
@@ -6,13 +8,30 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatExpansionModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
   private fb = inject(FormBuilder);
   private emailService = inject(EmailService);
+
+  faqs = [
+    {
+      question: 'What types of painting services do you offer?',
+      answer: 'We provide both residential and commercial painting services, including interior and exterior projects.',
+      expanded: true
+    },
+    {
+      question: 'How do you determine pricing?',
+      answer: 'Pricing is based on the scope of the project, materials used, and the specific requirements of each job.',
+      expanded: false
+    },
+    {
+      question: 'What is your scheduling process?',
+      answer: 'We work with you to find a convenient time for your project, ensuring minimal disruption to your daily routine.'
+    }
+  ];
 
   contactForm: FormGroup = this.fb.group({
     name: [''],
